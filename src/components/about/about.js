33f58c162d82cd5bf29edbf03b20ae80dfc8c49d
Ethan Releford <90151318+ethanreleford@users.noreply.github.com>
2024-05-22 14:00:25 -0400

    update

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -10,12 +10,7 @@ function about() {
       My name is Ethan Releford, and I'm a senior majoring in Computer Science at the University of Central Florida.
       I enjoy creating small projects in various programming languages to expand my range of ideas. Currently, I'm
       actively seeking a software development internship for the summer of 2024. I anticipate my graduation to be
-      sometime between the summer and fall of 2024, depending on the opportunities that come my way.
-      </div>
-      <div className='desc descs'>
-      During my academic journey, I've had the privilege of immersing myself in the dynamic world of programming,
-      experimenting with a wide range of languages and technologies. This exploration has not only equipped me with
-      a solid foundation in computer science but also fostered my creativity and problem-solving skills.
+      fall of 2024.
       </div>
       <div className='desc descs'>
       Throughout my academic journey, I've consistently engaged in personal projects.. These projects span various domains, from
