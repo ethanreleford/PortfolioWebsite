a66365d5a907f9b9afdfde5e61b18e4f675e9cd7
Ethan Releford <ethanrele@gmail.com>
2024-08-12 11:17:01 -0400

    update website

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -6,16 +6,18 @@ function about() {
     <div id = 'about'>
     <div className='container3 info'>
       <h1 className='mainTitle'>About</h1>
-      <div className='desc descs'>
+      <div className='projContainer'>
+      <h3 className='descs'>
       My name is Ethan Releford, and I'm a senior majoring in Computer Science at the University of Central Florida.
       I enjoy creating small projects in various programming languages to expand my range of ideas. Currently, I'm
       actively seeking a software development internship for the summer of 2024. I anticipate my graduation to be
       fall of 2024.
-      </div>
-      <div className='desc descs'>
+      </h3>
+      <h3 className='descs'>
       Throughout my academic journey, I've consistently engaged in personal projects.. These projects span various domains, from
        web development and mobile app creation to machine learning and data analysis. This diverse experience has allowed me to
       adapt to different challenges and cultivate an understanding of software development.
+      </h3>
       </div>
     </div>
     </div>
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -11,7 +11,7 @@ function home() {
         <h1 className='name'>Ethan Releford</h1>
         <h2 className='subtitle'>Computer Science Student at the University of Central Florida</h2>
         <h2 className='subtitle'>Orlando, FL - ethanrele@gmail.com</h2>
-        <h3 className='desc'>I am an enthusiastic Software Developer who enjoys building software and solving problems</h3>
+        <h3 className='desc'>I am a current Undergraduate Computer Science student who enjoys building software projects</h3>
 
         <a href = "https://github.com/ethanreleford" target="_blank" rel ="noreferrer"><img className = "professional" src = {imageList[2]} alt= {"atom"} /></a>
         <a href = "https://www.linkedin.com/in/ethan-releford/" target="_blank"  rel ="noreferrer"><img className = "professional" src = {imageList[8]} alt= {"atom"}/></a>
